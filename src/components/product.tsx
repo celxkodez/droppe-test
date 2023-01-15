38272987e563af894569d5af53c13931c09f7a62
Celestine Stephen <decele2011@gmail.com>
2023-01-15 18:44:38 +0100

    refactor: code formating on product component into something readable

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -4,38 +4,51 @@ import {FaStar} from "react-icons/fa";
 
 export const Product: React.FC<{
     index: number;
-    product: { title: string; description: string; price: number; isFavorite: boolean; rating: {rate: number; count: number} };
+    product: {
+        title: string;
+        description: string;
+        price: number;
+        isFavorite: boolean;
+        rating: {
+            rate: number;
+            count: number
+        }
+    };
     toggleFavorite: (title: string) => void;
 }> = ({ product, toggleFavorite }) => {
     const {product: productClass, productBody, actionBarItem, actionBarItemLabel} = styles
     // Problem: Now product title can be too long, I just put overflowX as fix now
     return (
         <span className={productClass} style={{display: 'inline-block', overflowX: 'scroll', float: 'none', clear: 'both'}}>
-      <span className={styles['product-title']} style={{overflowX: 'hidden'}}>{product.title}</span>
+            <span className={styles['product-title']} style={{overflowX: 'hidden'}}>{product.title}</span>
 
-      <p><strong>Rating: {product.rating ? `${product.rating.rate}/5` : ''}</strong></p>
+            <p>
+                <strong>Rating: {product.rating ? `${product.rating.rate}/5` : ''}</strong>
+            </p>
 
-      <p><b>Price: ${+product.price}</b></p>
+            <p>
+                <b>Price: ${+product.price}</b>
+            </p>
 
-      <p className={productBody}>
-        <span><b>Description:</b></span>
-        <br/>
-          {product.description}
-     </p>
+            <p className={productBody}>
+                <span><b>Description:</b></span>
+                <br/>
+                {product.description}
+            </p>
 
-      <span className={styles['action_bar']} style={{display: 'table', width: "100%"}}>
-        <span
-            className={`${actionBarItem} ${
-                product.isFavorite ? "active" : ""
-            }`}
-            role="button"
-            onClick={() => {
-                toggleFavorite(product.title);
-            }}
-        >
-          <FaStar /> <span className={actionBarItemLabel}>{((product.isFavorite)) ? 'Remove from favorites' : 'Add to favorites'}</span>
+            <span className={styles['action_bar']} style={{display: 'table', width: "100%"}}>
+                <span
+                    className={`${actionBarItem} ${
+                        product.isFavorite ? "active" : ""
+                    }`}
+                    role="button"
+                    onClick={() => {
+                        toggleFavorite(product.title);
+                    }}
+                >
+                    <FaStar /> <span className={actionBarItemLabel}>{((product.isFavorite)) ? 'Remove from favorites' : 'Add to favorites'}</span>
+                </span>
+            </span>
         </span>
-      </span>
-    </span>
     );
 };
